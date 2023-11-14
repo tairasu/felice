7da3e9b9a633bb0354940a7b07ef8b52a6639129
aomizu <marco.tylus24@gmail.com>
2023-11-14 21:32:06 +0100

    minimized to theme

---
+++ 404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+<body class="antialiased">
+	404
+</body>
+</html>
