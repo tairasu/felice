2411e5adc7f8a9ecc97a9b30582eaaf5bfc6730c
aomizu <marco.tylus24@gmail.com>
2024-03-16 16:19:36 +0100

    updated 404 page

--- 404.php
+++ 404.php
@@ -5,9 +5,14 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+	<?php wp_head();
+	get_header(); ?>
 </head>
 <body class="antialiased">
-	404
+	<div class="flex-col mx-auto my-auto">
+		<p class="text-4xl mx-auto text-center">ページが見つかりませんでした</p>
+		<p class="text-2xl mx-auto text-center">お探しのページはURLが間違っているか、削除された可能性があります。</p>
+	</div>
+	
 </body>
 </html>
