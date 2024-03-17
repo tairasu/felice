3d976c4f1b94843172e95fb4dfbc7710fd34ce41
aomizu <marco.tylus24@gmail.com>
2024-03-17 17:50:57 +0100

    removed unused code

--- js/app.js
+++ js/app.js
@@ -7,15 +7,6 @@
   \*****************************/
 /***/ (() => {
 
-// Navigation toggle
-window.addEventListener('load', function () {
-  var main_navigation = document.querySelector('#primary-menu');
-  document.querySelector('#primary-menu-toggle').addEventListener('click', function (e) {
-    e.preventDefault();
-    main_navigation.classList.toggle('hidden');
-  });
-});
-
 /***/ }),
 
 /***/ "./resources/css/app.css":
