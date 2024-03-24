d3359ed2d92abb973383b0973b23ed3a9f3d7cc6
aomizu <marco.tylus24@gmail.com>
2024-03-24 10:26:13 +0100

    added single post handling

--- single.php
+++ single.php
@@ -1,28 +1,23 @@
-<?php include 'header.php'; ?>
+<?php get_header(); ?>
 
-	<div class="container mx-auto">
-	<?php if ( have_posts() ) : ?>
+<main role="main">
+    <?php while (have_posts()) : the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" class="p-4 bg-white/25 shadow-lg rounded-lg ring-1 ring-yellow-800 w-fit mx-auto mb-8 mt-6" <?php post_class(); ?>>
+            <header class="entry-header">
+                <?php the_title('<h1 class="entry-title font-bold">', '</h1>'); ?>
+            </header>
+			
+			<div class="flex items-start">
+    			<div class="text-yellow-900 bg-orange-50 rounded-full ring-1 p-2 ring-yellow-800 mb-8">
+        			<?php echo get_the_date('Y年 n月 j日'); ?>
+    			</div>
+			</div>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			?>
+            <div class="entry-content text-justify">
+                <?php the_content(); ?>
+            </div>
+        </article>
+    <?php endwhile; ?>
+</main>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
-
-			<?php
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-			?>
-
-		<?php endwhile; ?>
-
-	<?php endif; ?>
-
-
-	</div>
-
-<?php
-get_footer();
+<?php get_footer(); ?>
